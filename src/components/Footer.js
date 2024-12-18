754b8edbc38bb155ba2bed94f27ad9b3846d4721
Gouri Biswas <gouribiswas011@gmail.com>
2024-12-18 17:50:12 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Icon} from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/fire-alert'
+import thunderIcon from '@iconify/icons-mdi/thunder'
+
+const Footer = () => {
+  return (
+    <div className='footer'><h2>Made By Gouri Biswas <Icon icon={thunderIcon} /></h2></div>
+  )
+}
+
+export default Footer
